a647e58d39b11ad29baa48c9afe65e2b4a3d94e4
Benjamin Schwartz <bs4308@nyu.edu>
2023-01-20 02:07:14 -0500

    Add project info

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,8 +19,16 @@ const btn2 = document.getElementById(`myBtn2`);
 const modal3 = document.getElementById(`myModal3`);
 const btn3 = document.getElementById(`myBtn3`);
 
+const modal4 = document.getElementById(`myModal4`);
+const btn4 = document.getElementById(`myBtn4`);
+
+const modal5 = document.getElementById(`myModal5`);
+const btn5 = document.getElementById(`myBtn5`);
+
 btn0.onclick = function() {
 	modal0.style.display = "block";
+	// document.getElementById.style.opacity=.1;
+
 }
 btn1.onclick = function() {
 	modal1.style.display = "block";
@@ -30,6 +38,14 @@ btn2.onclick = function() {
 }
 btn3.onclick = function() {
 	modal3.style.display = "block";
+}
+
+btn4.onclick = function() {
+	modal4.style.display = "block";
+}
+
+btn5.onclick = function() {
+	modal5.style.display = "block";
 }
 
 // When the user clicks anywhere outside of modal, close it
@@ -45,6 +61,12 @@ window.onclick = function(event) {
 	}
 	if (event.target == modal3 ) {
 		modal3.style.display = "none";
+	}
+	if (event.target == modal4 ) {
+		modal4.style.display = "none";
+	}
+	if (event.target == modal5 ) {
+		modal5.style.display = "none";
 	}
 }
 
